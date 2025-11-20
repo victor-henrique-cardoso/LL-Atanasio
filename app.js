9c469e265065f64f20cffe6bcedadaf66ab4f459
victor-henrique-cardoso <victorcardosoferreirahenrique@gmail.com>
2025-11-20 10:59:54 -0400

    coloquei os nomes e preço certo falta a descrição

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-const vendorPhone = '556984799039';
+const vendorPhone = '556993807246';
 
 const products = [
   {id:1,
-  name:'SlimFit Caps',
-  price:129.90,
+  name:'Pawer Whey',
+  price:230.00,
   category:'Suplementos',
   desc:'Cápsulas naturais para controle de apetite.', 
   image:'imagems/1be8328c-5345-451f-b39d-a4df70591c3d.png'
@@ -12,32 +12,32 @@ const products = [
 
   {
   id:2, 
-  name:'Chá Detox', 
-  price:49.90, 
+  name:'Power Detox', 
+  price:180.00, 
   category:'Chás', 
   desc:'Blend natural para desintoxicar e acelerar metabolismo.', image:'imagems/3835b8b6-046a-4c0e-966e-1237759599a2.png'
   },
 
   {
     id:3, 
-    name:'Belt Slim', 
-    price:79.90, 
+    name:'Power Silver', 
+    price:280.00, 
     category:'Acessórios', 
     desc:'Cinta modeladora para uso durante exercícios.',image:'./imagems/388f6162-2a20-411f-885d-484922cb367c.png'
   },
 
   {
     id:4, 
-    name:'Shake Proteico', 
-    price:99.90, 
+    name:'Power Colágeno', 
+    price:150.00, 
     category:'Suplementos', 
     desc:'Shake com proteínas e fibras para saciedade.', image:'imagems/50bc4467-1c04-4ae8-96af-b9f1fca4dbc3.png'
   },
 
   {
     id:5, 
-    name:'Chá Emagrecedor', 
-    price:39.90, 
+    name:'Power Vitamina', 
+    price:150.00, 
     category:'Chás', 
     desc:'Chá termogênico com sabor agradável.', 
     image:'imagems/6720eca7-4f7d-4ee3-9f13-528069ae725b.png'
@@ -45,8 +45,8 @@ const products = [
 
   {
     id:6, 
-    name:'Tapete Yoga', 
-    price:59.90, 
+    name:'Power Creatina', 
+    price:150.00, 
     category:'Acessórios', 
     desc:'Tapete antiderrapante para treino em casa.', 
     image:'imagems/6805a1a3-1290-4980-b83e-25d8ca61cfd0.png'
@@ -54,15 +54,15 @@ const products = [
 
   {
     id:7,
-    name:'Pré Treino',
-    price:119.90,
+    name:'Power Golden',
+    price:260.00,
     category:'Suplementos',
     desc:'Termogênico avançado para queima de gordura.',
     image:'imagems/6d64c61b-6212-4e8c-8212-cb2948f80cbd.png'
   },
   {
     id:8,
-    name:'Chá Verde Premium',
+    name:'Power Creatina Goma',
     price:59.90,
     category:'Chás',
     desc:'Chá verde de alta qualidade para acelerar o metabolismo.',
@@ -70,27 +70,43 @@ const products = [
   },
   {
     id:9,
-    name:'Rolo de Espuma',
-    price:89.90,
+    name:'Power Pump',
+    price:150.00,
     category:'Acessórios',
     desc:'Rolo para liberação miofascial e alívio muscular.',
     image:'imagems/d656bede-5f7d-4bd3-8c6b-8ab0345eaa7e.png'
   },
   {
     id:10,
-    name:'Whey Protein Isolado',
-    price:149.90,
+    name:'Power Coffee',
+    price:180.00,
     category:'Suplementos',
     desc:'Proteína isolada para recuperação muscular rápida.',
     image:'imagems/power coffee.png'
   },
   {
     id:11,
-    name:'Chá de Hibisco',
-    price:29.90,
+    name:'Power Reedy',
+    price:300.00,
     category:'Chás',
     desc:'Chá de hibisco 100% natural para auxiliar na perda de peso.',
     image:'imagems/Power reedy.png'
+  },
+  {
+    id:12,
+    name:'Power Omega 3',
+    price:120.00,
+    category:'Acessórios',
+    desc:'Corda de pular ajustável para exercícios cardiovasculares.',
+    image:'imagems/omega 3.png'
+  },
+  {
+    id:13,
+    name:'Power Diamond',
+    price:290.00,
+    category:'Suplementos',
+    desc:'Aminoácidos essenciais para recuperação muscular.',
+    image:'imagems/power diamante.png'
   }
 ];
 
